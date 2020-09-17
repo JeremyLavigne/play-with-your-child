41e6ed75a7737f5c6961144f2ad867bd0186329f
JeremyLavigne <lavigne.jeremy@post.com>
2020-09-17 11:56:54 +0200

    set main container responsive with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
-import React, {useState} from 'react'
+import React from 'react'
 
 
 const App = () => {
-  const [counter, setCounter] = useState(0)
-  const [values, setValues] = useState([])
-
-  const handleClick = () => {
-    setCounter(counter + 1)
-    setValues(values.concat(counter))
-  }
 
   return (
-    <div className="container">
-      hello webpack {counter} clicks
-      <button onClick={handleClick}>
-        press
-      </button>
+    <div className="container bg-primary d-flex flex-column justify-content-center" style={{ height: '100vh' }}>
+
+      <div className="container bg-info">
+        <p>Play with your child and let him/her decide if I can be a good developer :-)</p>
+      </div>
+
+      <div className="container">
+        <div className="row bg-primary d-flex justify-content-around">
+
+          <div className="col-5 col-md-3 m-1 bg-success">
+            1
+          </div>
+          <div className="col-5 col-md-3 m-1 bg-success">
+            2
+          </div>
+          <div className="col-5 col-md-3 m-1 bg-success">
+            3
+          </div>
+          <div className="col-5 col-md-3 m-1 bg-success">
+            4
+          </div>
+          <div className="col-5 col-md-3 m-1 bg-success">
+            5
+          </div>
+          <div className="col-5 col-md-3 m-1 bg-success">
+            6
+          </div>
+
+        </div>
+      </div>
     </div>
   )
 }
